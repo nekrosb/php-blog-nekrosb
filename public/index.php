e25bfc950b15237fae780ab75e157e681b69ce89
nekrosb <makarov.nikita.mn@gmail.com>
2026-03-13 15:05:42 +0100

    feat: Load posts from the database
    added new method getPosts to class database to load posts from database
    (index.php) add code for show loaded posts from database and script for heeden and show content of post

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require "../src/classes/working-whith-db.php";
 $db = Database::getInstance();
+$posts = $db->getPosts();
 
 
 ?>
@@ -21,48 +22,43 @@ $db = Database::getInstance();
 
 
     <div class="post-container">
-        <div class="post">
-            <h2>title of post</h2>
-            <img src="" alt="">
+        <?php foreach ($posts as $post): ?>
 
-            <strong>created at</strong>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia non mollitia eveniet beatae. Totam asperiores ut aliquid repellat reiciendis deserunt quasi praesentium dignissimos ipsam quia mollitia molestiae, tempora amet maiores dolor illum, quas quis? Nisi quasi exercitationem asperiores fuga, animi reiciendis excepturi quas dignissimos dicta incidunt explicabo dolore nam, magnam dolorem quo atque ratione quam. Tempora error delectus pariatur iusto cum asperiores blanditiis rerum minima soluta esse animi voluptatibus natus quas, voluptas omnis maiores quibusdam at? Fuga error est alias reiciendis similique autem, dolor commodi impedit quibusdam recusandae. Praesentium fugiat consequatur dolorem enim, nam laudantium nihil quo aspernatur, doloribus rem quibusdam velit? Labore aspernatur error architecto minima commodi accusamus eveniet ipsam, magnam minus molestiae cum incidunt voluptatum distinctio explicabo molestias necessitatibus voluptatem soluta a praesentium optio! Consequatur eum mollitia maxime obcaecati dolore, esse et? Repellat earum deserunt, facere iusto doloribus ad a eos harum nam asperiores illum inventore distinctio.
-            </p>
-        </div>
+            <div class="post">
 
+                <h2><?= $post['title'] ?></h2>
 
-        <div class="post">
-            <h2>title of post</h2>
-            <img src="" alt="">
+                <img src="<?= $post['image'] ?>" class="post-image">
 
-            <strong>created at</strong>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia non mollitia eveniet beatae. Totam asperiores ut aliquid repellat reiciendis deserunt quasi praesentium dignissimos ipsam quia mollitia molestiae, tempora amet maiores dolor illum, quas quis? Nisi quasi exercitationem asperiores fuga, animi reiciendis excepturi quas dignissimos dicta incidunt explicabo dolore nam, magnam dolorem quo atque ratione quam. Tempora error delectus pariatur iusto cum asperiores blanditiis rerum minima soluta esse animi voluptatibus natus quas, voluptas omnis maiores quibusdam at? Fuga error est alias reiciendis similique autem, dolor commodi impedit quibusdam recusandae. Praesentium fugiat consequatur dolorem enim, nam laudantium nihil quo aspernatur, doloribus rem quibusdam velit? Labore aspernatur error architecto minima commodi accusamus eveniet ipsam, magnam minus molestiae cum incidunt voluptatum distinctio explicabo molestias necessitatibus voluptatem soluta a praesentium optio! Consequatur eum mollitia maxime obcaecati dolore, esse et? Repellat earum deserunt, facere iusto doloribus ad a eos harum nam asperiores illum inventore distinctio.
-            </p>
-        </div>
+                <div class="content">
+                    <?= nl2br($post['content']) ?>
+                </div>
 
+                <button class="toggle-btn">read more</button>
 
-        <div class="post">
-            <h2>title of post</h2>
-            <img src="" alt="">
+            </div>
 
-            <strong>created at</strong>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia non mollitia eveniet beatae. Totam asperiores ut aliquid repellat reiciendis deserunt quasi praesentium dignissimos ipsam quia mollitia molestiae, tempora amet maiores dolor illum, quas quis? Nisi quasi exercitationem asperiores fuga, animi reiciendis excepturi quas dignissimos dicta incidunt explicabo dolore nam, magnam dolorem quo atque ratione quam. Tempora error delectus pariatur iusto cum asperiores blanditiis rerum minima soluta esse animi voluptatibus natus quas, voluptas omnis maiores quibusdam at? Fuga error est alias reiciendis similique autem, dolor commodi impedit quibusdam recusandae. Praesentium fugiat consequatur dolorem enim, nam laudantium nihil quo aspernatur, doloribus rem quibusdam velit? Labore aspernatur error architecto minima commodi accusamus eveniet ipsam, magnam minus molestiae cum incidunt voluptatum distinctio explicabo molestias necessitatibus voluptatem soluta a praesentium optio! Consequatur eum mollitia maxime obcaecati dolore, esse et? Repellat earum deserunt, facere iusto doloribus ad a eos harum nam asperiores illum inventore distinctio.
-            </p>
-        </div>
+        <?php endforeach; ?>
 
+        <script>
+            document.querySelectorAll(".toggle-btn").forEach(button => {
 
-        <div class="post">
-            <h2>title of post</h2>
-            <img src="" alt="">
+                        button.addEventListener("click", function() {
 
-            <strong>created at</strong>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At officia non mollitia eveniet beatae. Totam asperiores ut aliquid repellat reiciendis deserunt quasi praesentium dignissimos ipsam quia mollitia molestiae, tempora amet maiores dolor illum, quas quis? Nisi quasi exercitationem asperiores fuga, animi reiciendis excepturi quas dignissimos dicta incidunt explicabo dolore nam, magnam dolorem quo atque ratione quam. Tempora error delectus pariatur iusto cum asperiores blanditiis rerum minima soluta esse animi voluptatibus natus quas, voluptas omnis maiores quibusdam at? Fuga error est alias reiciendis similique autem, dolor commodi impedit quibusdam recusandae. Praesentium fugiat consequatur dolorem enim, nam laudantium nihil quo aspernatur, doloribus rem quibusdam velit? Labore aspernatur error architecto minima commodi accusamus eveniet ipsam, magnam minus molestiae cum incidunt voluptatum distinctio explicabo molestias necessitatibus voluptatem soluta a praesentium optio! Consequatur eum mollitia maxime obcaecati dolore, esse et? Repellat earum deserunt, facere iusto doloribus ad a eos harum nam asperiores illum inventore distinctio.
-            </p>
-        </div>
+                                const content = this.previousElementSibling;
+
+                                content.classList.toggle("open");
+
+                                if (content.classList.contains("open")) {
+                                    this.textContent = "read less";
+                                } else {
+                                    this.textContent = "read more";
+                                }
+
+                            });
+
+                        });
+        </script>
 
 
     </div>
--- src/classes/working-whith-db.php
+++ src/classes/working-whith-db.php
@@ -72,4 +72,10 @@ class Database
             $this->createDB();
         }
     }
+
+    public function getPosts(): array
+    {
+        $stmt = $this->pdo->query("SELECT title, image, content FROM posts ORDER BY created_at DESC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
