e7cc691d3ea03742e186e0377a96d5233ba955eb
nekrosb <makarov.nikita.mn@gmail.com>
2026-03-24 11:55:08 +0100

    fix(logout.php): use check server request method for logout

--- public/logout.php
+++ public/logout.php
@@ -1,20 +1,23 @@
 <?php
 session_start();
-$_SESSION = [];
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
-    );
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $_SESSION = [];
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+    session_destroy();
+
+
+    header("Location: /");
+    exit();
 }
-session_destroy();
-
-
-header("Location: /");
-exit();
